73d476ce5994eb9e9475b555861da1493cc0b1f2
Ahmad <ahmad.alsayed@dci-student.org>
2025-03-21 11:49:56 +0100

    AddingBackGround

--- src/components/VrGlasses.jsx
+++ src/components/VrGlasses.jsx
@@ -2,6 +2,7 @@ import image1 from "./../assets/image1.jpg";
 import image2 from "./../assets/image2.jpg";
 import image3 from "./../assets/image3.jpg";
 import image4 from "./../assets/image4.jpg";
+import backGround  from "./../assets/background.jpg"
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { useState } from "react";
